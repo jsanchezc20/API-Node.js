ed280a054998863dfbf14a5b55dc2eaa95373781
Jaime Sánchez Cárcel <jsanchezc20@ilg.cat>
2022-05-12 01:24:03 +0200

    JSON /products

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ const appObj = expressObj();
 const intPort = 3000;
 
 appObj.get("/", (req, res) => res.send("Hello World!"));
+appObj.get("/products", (req, res) => {
+	const products = [
+		{
+			id: 1,
+			name: "hammer",
+		},
+		{
+			id: 2,
+			name: "screwdriver",
+		},
+		{
+			id: 3,
+			name: "wrench",
+		}, // <- Em sembla que aquesta coma sobra
+	];
+	res.json(products);
+});
+
 appObj.listen(intPort, () => console.log(`Example app listening on port ${intPort}`));
